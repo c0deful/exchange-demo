a661d1dd923a1ba715cff4ca2fc5eebc422b74b1
Dominik Plewa <dominik@codeful.it>
2021-04-08 20:33:38 +0200

    Add minimum age validation

--- user-service/src/main/java/it/codeful/exchange/userservice/validation/MinAgePESEL.java
+++ user-service/src/main/java/it/codeful/exchange/userservice/validation/MinAgePESEL.java
@@ -1,7 +1,7 @@
 package it.codeful.exchange.userservice.validation;
 
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
+import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
--- user-service/src/main/java/it/codeful/exchange/userservice/validation/MinAgePESELValidator.java
+++ user-service/src/main/java/it/codeful/exchange/userservice/validation/MinAgePESELValidator.java
@@ -1,8 +1,8 @@
 package it.codeful.exchange.userservice.validation;
 
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
 
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
@@ -15,14 +15,15 @@ public class MinAgePESELValidator implements ConstraintValidator<MinAgePESEL, Ch
     }
 
     @Override
-    public boolean isValid(CharSequence charSequence, ConstraintValidatorContext constraintValidatorContext) {
+    public boolean isValid(CharSequence charSequence, ConstraintValidatorContext context) {
         int century = 19 + Integer.parseInt(charSequence, 2, 3, 10) / 2;
         int year = Integer.parseInt(charSequence, 0, 2, 10);
         int month = Integer.parseInt(charSequence, 2, 4, 10) % 20;
         int day = Integer.parseInt(charSequence, 4, 6, 10);
         String dateText = String.format("%02d%02d%02d%02d", century, year, month, day);
         LocalDate birthDate = LocalDate.parse(dateText, DateTimeFormatter.BASIC_ISO_DATE);
-        return Period.between(birthDate, LocalDate.now()).getYears() >= 18;
+        LocalDate now = LocalDate.now(context.getClockProvider().getClock());
+        return Period.between(birthDate, now).getYears() >= 18;
     }
 }
 
