5e63cfb4629b891bfff444735269df25b6b6b7ce
Dominik Plewa <dominik@codeful.it>
2021-04-11 23:19:15 +0200

    Cleanup code in gateway service

--- gateway-service/src/main/java/it/codeful/exchange/gatewayservice/account/AccountClient.java
+++ gateway-service/src/main/java/it/codeful/exchange/gatewayservice/account/AccountClient.java
@@ -3,8 +3,6 @@ package it.codeful.exchange.gatewayservice.account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
